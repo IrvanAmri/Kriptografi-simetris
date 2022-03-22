f61b914888dddff19931fea8a405a04c0c6195a8
IrvanAmri <72341169+IrvanAmri@users.noreply.github.com>
2022-03-22 21:12:10 +0700

    Add service ketiga
    service ketiga: konversi string ke array of MatBlock
    juga menambah fungsi visualisasi untuk debugging

--- kriptografi2/src/main/java/com/tugas2kriptografi/Services.java
+++ kriptografi2/src/main/java/com/tugas2kriptografi/Services.java
@@ -1,5 +1,54 @@
 package com.tugas2kriptografi;
 
 public class Services {
-    
+
+    //3. Konversi String ke Array of MatBlock
+    public static MatBlock[] stringToMatBlockArray(String str){
+        int l = str.length();
+        int n = (int)Math.ceil((double)l/16);
+        MatBlock[] arr = new MatBlock[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = new MatBlock();
+        }
+
+        char karakter[] = str.toCharArray();
+        int index = 0; // digunakan untuk merujuk elemen pada karakter
+        
+        //perulangan untuk setiap anggota dari arr
+        for(int i = 0; i<n; i++){
+            //perulangan untuk tiap row
+            for(int j = 0; j<4; j++){
+                //perulangan untuk tiap collumn
+                for(int k = 0; k<4; k++){
+                    if(index<l){
+                        arr[i].writeBlock(j,k,(int)karakter[index]);
+                    }
+                    //padding
+                    else{
+                        arr[i].writeBlock(j, k, 32);
+                    }
+                    index++;
+                }
+            }
+        }
+        return arr;
+    }
+
+    //visualisasi bukan suatu service, hanya untuk debugging
+    public static void visualisasiArrayOfMatBlock(MatBlock[] arr){
+        int n = arr.length;
+        for(int i = 0; i<n; i++){
+            System.out.println("MathBlock["+i+"] :");
+            //perulangan untuk tiap row
+            for(int j = 0; j<4; j++){
+                //perulangan untuk tiap collumn
+                for(int k = 0; k<4; k++){
+                    System.out.print(arr[i].readBlock(j, k)+" ");
+                }
+                System.out.println("");
+            }
+            System.out.println("");
+            System.out.println("");
+        }
+    }
 }
